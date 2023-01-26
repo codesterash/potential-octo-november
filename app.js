cd19bb38254a54284b720f9175ecf26ff678191b
Paruzynski <aparuzynski@una.edu>
2023-01-26 16:40:35 -0600

    client to node data passed using form action

--- app.js
+++ app.js
@@ -19,18 +19,23 @@ res.send(`<h3> Hi, ${ myName } </h3>`)
 })
 
 app.get('/show', (req, res) => {
-
     // res.sendFile('index.html');
     res.sendFile('index.html' , { root : __dirname});
 })
 
-app.get('/ejs', (req,res) => {
+console.log("before app dot get to slash ejs", myName);
 
+app.get('/ejs', (req,res) => {
+  console.log("in /ejs before render:", myName);
     //ejs stuff goes here. 
 
     // use res.render to load up an ejs view file
-    res.render('index');
+    res.render('index', { myName: myName }); //left is ejs, right is node
+    console.log("after res render on /ejs", myName);
+})
 
+app.get('/name', (req,res) => {
+  console.log("in get to slash name", req.query.ejsFormName);
 })
 
 console.log('in the node console');
